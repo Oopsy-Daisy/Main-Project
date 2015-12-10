eb990dc71adb348988503445f191d4aa3cd775e7
andrewzung <andrewjzung@gmail.com>
2015-12-10 17:32:47 -0500

    Framework Comments
    Addition of guidance and framework for code based on the OOP Class
    Diagram (See PDF).

--- Flower.java
+++ Flower.java
@@ -10,5 +10,8 @@ import java.awt.event.*;
 import java.awt.Graphics2D;
 
 public class Flower
-{   
+{
+  //Flower variables including the colour height and health
+  //Methods for flower, mainly idle, grow and die
+  
 }
