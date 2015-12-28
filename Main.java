4ef1d4ee92b78061e62955f56a0034d25d4c4e24
Farmgokoro <nakman02@gmail.com>
2015-12-28 17:00:56 -0500

    Update Main.java
     I DID STUFF

--- Main.java
+++ Main.java
@@ -7,14 +7,50 @@ import java.awt.image.BufferedImage;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.*;
-import java.awt.Graphics2D;
 
-public class Main
+public class Main extends JPanel
 {   
   //Overall Variables from Class Diagram
+  private Typing type = new Typing(this);
+  
+  public Main ()
+  {
+    addKeyListener(new KeyListener() {
+      @Override
+      public void keyTyped(KeyEvent e) {
+      }
+      @Override
+      public void keyReleased(KeyEvent e) {
+        type.keyReleased(e);
+      }
+      @Override
+      public void keyPressed(KeyEvent e) {
+        type.keyPressed(e);
+      }
+    });
+    setFocusable(true); 
+  }
+  
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    type.paint(g2d);
+  }
   //Option Methods
   //Methods for the interaction of sub methods; final integration step
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) throws InterruptedException {
+    JFrame frame = new JFrame("Animation");
+    Main ba = new Main();
+    frame.add(ba);
+    frame.setSize(1280, 984);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    while (true) {
+      ba.repaint();
+      Thread.sleep(10);
+    }
   }
 }
