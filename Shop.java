4d2ff95e8d97d28d8446d719b12fb2d40d7b3cf9
andrewzung <andrewjzung@gmail.com>
2015-12-28 08:47:33 -0500

    Foundation of Shop Class

--- Shop.java
+++ Shop.java
@@ -8,10 +8,24 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.*;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 public class Shop
 {  
   //ArrayList of Visible and Unavailable Items
   //Methods to display items
   //Method to sell items and to buy back items from user
+  ArrayList<String> availableItems = new ArrayList<String>();
+  ArrayList<String> unavailableItems = new ArrayList<String>();
+  
+  public Shop(int userLevel)
+  {
+    unavailableItems.add("greenFlower.png");
+    unavailableItems.add("blueFlower.png");
+    unavailableItems.add("orangeFlower.png");
+    unavailableItems.add("purpleFlower.png");
+    unavailableItems.add("fertilizer.png");
+  }
+  
+  
 }
