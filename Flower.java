7635982807c5fbd2595cc973e1cc73f2f921e3f9
KavyaRaman <kavya.ramesh.98@gmail.ca>
2016-01-13 12:51:10 -0500

    Flower.java
    mainly skeleton code, needs extensive edits

--- Flower.java
+++ Flower.java
@@ -9,9 +9,27 @@ import java.awt.event.KeyListener;
 import java.awt.event.*;
 import java.awt.Graphics2D;
 
-public class Flower
+public class Flower extends JPanel
 {
   //Flower variables including the colour height and health
+  private String colour;
+  private int height;
+  private int health;
   //Methods for flower, mainly idle, grow and die
-  
+  public void idle(){
+    //sway animation????
+  }
+  public void grow(){
+    String c = colour;
+    int health = 100;
+    @Override
+public void paint(Graphics g) {
+Graphics2D g2d = (Graphics2D) g;
+g2d.setColor(Color.c);//GET COLOUR FROM SEED
+
+
+  }
+  private void die(){
+    //remove flower from end list
+  }
 }
