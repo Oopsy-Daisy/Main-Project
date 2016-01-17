d6ef51b77f5e90af3e8c0004986883193c8cfbfd
andrewzung <andrewjzung@gmail.com>
2016-01-17 14:28:03 -0500

    Setting up of the Shop

---
+++ Item.java
@@ -0,0 +1,53 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.*;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.BoxLayout;
+import javax.swing.Box;
+import javax.swing.BorderFactory;
+import javax.swing.border.Border;
+import javax.swing.JTabbedPane;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.beans.*; //Property change stuff
+import java.util.Timer;
+import java.util.TimerTask;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+
+public class Item
+{
+  private BufferedImage img = null;
+  private String fileLocation = "";
+  private int x = 0;
+  private int y = 0;
+  
+  public Item(String fileLocation)
+  {
+    this.fileLocation = fileLocation;
+    try
+    {
+      img = ImageIO.read(new File(fileLocation));
+    } catch (IOException e)
+    {
+      System.out.println("No Image");
+    }
+  }
+  
+  public void paint(Graphics2D g2d)
+  {
+    g2d.drawImage(img, x, y, null);
+  }
+}
