4003be353ef9d9ae12146bf5ba312ebd7e1a5810
andrewzung <andrewjzung@gmail.com>
2016-01-17 14:28:03 -0500

    Setting up of the Shop

--- Shop.java
+++ Shop.java
@@ -17,15 +17,25 @@ public class Shop
   //ArrayList of Visible and Unavailable Items
   //Methods to display items
   //Method to sell items and to buy back items from user
-  ArrayList<String> availableItems = new ArrayList<String>();
-  ArrayList<String> unavailableItems = new ArrayList<String>();
+  ArrayList<Item> availableItems = new ArrayList<Item>();
+  ArrayList<Item> unavailableItems = new ArrayList<Item>();
   
   public Shop(int userLevel)
   {
-    unavailableItems.add("greenFlower.png");
-    unavailableItems.add("blueFlower.png");
-    unavailableItems.add("orangeFlower.png");
-    unavailableItems.add("pinkFlower.png");
-    unavailableItems.add("fertilizer.png");
+    availableItems.add(new Item("greenFlower.png"));
+    unavailableItems.add(new Item("blueFlower.png"));
+    unavailableItems.add(new Item("orangeFlower.png"));
+    unavailableItems.add(new Item("pinkFlower.png"));
+    availableItems.add(new Item("fertilizer.png"));
+  }
+  
+  public void paint(Graphics2D g) {
+    
+    BufferedImage img = null;
+    try {
+      img = ImageIO.read(new File("strawberry.jpg"));
+    } catch (IOException e) {
+    } 
+    
   }
 }
