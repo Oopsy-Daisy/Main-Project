de0855f073685a116b88af7b05d0ad4ce0fc96f9
KavyaRaman <kavya.ramesh.98@gmail.ca>
2016-01-18 14:18:32 -0500

    added comments

--- Garden.java
+++ Garden.java
@@ -11,18 +11,22 @@ import java.awt.Graphics2D;
 
 public class Garden extends JPanel
 {
- List<String> seedsAvailable = new ArrayList<String>();
- List<int> plot = new ArrayList<int>();
+ List<String> seedsAvailable = new ArrayList<String>();//each object has a colour, null xy
+ List<int> plot = new ArrayList<int>();//lists all the possible plot points, each object has an x,y
  //list of flowers and their properties, can be accessed by flower class
  
  public void plantSeed(String seedType, int location){
    //plant seed from available seeds in next avail plot
    if(seedsAvailable.length()>0)
    {
-   //plant next seed
+   //plant next seed in next available plot point, set boolean isAlive to true, paint unbloomed flower, set xy to plot location
+   }
  }
  public void useFertilizer(Flower flower){
    //force grow flower
  } 
-
+ public void background(){
+   //paints the garden, shows plot points, spots for text
+ }
+   
 }
