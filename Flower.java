85d600f065b3388b8ffc0a0915c10d5c66ce8695
KavyaRaman <kavya.ramesh.98@gmail.ca>
2016-01-18 14:17:58 -0500

    added comments

--- Flower.java
+++ Flower.java
@@ -13,23 +13,23 @@ public class Flower extends JPanel
 {
   //Flower variables including the colour height and health
   private String colour;
-  private int height;
-  private boolean alive;
-  List<Flower> flowers = new ArrayList<Flower>(plot, colour);
+  private boolean isGrown;
+  private boolean isAlive;
+  List<Flower> flowers = new ArrayList<Flower>(plot, colour);//grown flowers, plot and colour depends on seeds
   //Methods for flower, mainly idle, grow and die
   public void idle(){
     //sway animation????
   }
  
     @Override
-public void grow(Graphics g) {
+public void grow(Graphics g) {//grows flower one completed lesson after planting 
 Graphics2D g2d = (Graphics2D) g;
 g2d.drawRect(50, 50, 30, 30);//plot x,plot y, img)
 
 
   }
-  private void die(){
-    //remove flower from end list
+  public void die(){
+    //remove flower from end list, checks the accuracy of the lesson; if below 50%, kill flower
   }
 }
   
